cba7bb407c84aca7c8f959076cff514f93121b96
Sven Saeger <svtablet25@gmail.com>
2017-09-09 16:59:14 +0200

    Kommunikation Arduino

--- Kommunikation/communication.ino
+++ Kommunikation/communication.ino
@@ -1,20 +1,21 @@
-char textZumUebertragen[] = "test|";
+char text[] = "test|";
 
 void uebertragen() {
-  for(int i = 0; i<sizeof(textZumUebertragen); i++) {
-    Serial.println(textZumUebertragen[i], DEC);
+  for(int i = 0; i<(sizeof(text)); i++) {
+    Serial.println(text[i], DEC);
   }
 }
 
 void setup(){
     Serial.begin(9600);
-    delay(5000);
+    delay(2000);
     uebertragen();
 }
+
+int wert = 0;
  
 void loop(){
   delay(1000);
-  uebertragen();
-  delay(1000);
+  wert++;
   uebertragen();
 }
