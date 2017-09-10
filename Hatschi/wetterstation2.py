a9c42919207797001ec9fca10f1806117a77f77e
Sven Saeger <svtablet25@gmail.com>
2017-09-10 11:17:08 +0200

    Raspberry Pi fertig

--- Hatschi/wetterstation2.py
+++ Hatschi/wetterstation2.py
@@ -12,7 +12,8 @@ def ausgeben(daten):
         luftf.set("Luftfeuchtigkeit: "+dic["luftf"]+"%")
         luftdruck.set("Luftdruck: "+dic["luftd"]+"hPa")
         af.set("Absolute Feuchtigkeit: "+dic["af"]+"g/m^3")
-        gas.set("CO2-Gehalt: "+dic["gas"]+"ppm");
+        gas.set("CO2-Gehalt: "+dic["gas"]+"ppm")
+        
     except ValueError:
         print("ValueError: "+daten)
     except KeyError:
@@ -25,20 +26,20 @@ def ausgeben(daten):
 root = Tk()
 root.attributes("-fullscreen", True)
 root.config(bg = "dark grey")
-frameo = Frame(root)
+frameo = Frame(root)    
 frameu = Frame(root, bg = "dark grey")
 temp = StringVar()
-temp.set("Temperatur: 0°C")
+temp.set("Auf Daten warten...")
 luftf = StringVar()
-luftf.set("Luftfeuchtigkeit: 0%")
+luftf.set("")
 luftdruck = StringVar()
-luftdruck.set("Luftdruck: 0hPa")
+luftdruck.set("")
 af = StringVar()
-af.set("Absolute Feuchtigkeit: 0g/m^3")
+af.set("")
 gas = StringVar()
-gas.set("CO²-Gehalt: 0ppm")
+gas.set("")
 timeS = StringVar()
-timeS.set(time.strftime("%H:%M:%S", time.gmtime()))
+timeS.set("")
 
 
 
@@ -100,12 +101,14 @@ try:
     while True:
         try:
             line = s.readline().decode("ascii")
+            
+            ausgeben(line)
         except UnicodeDecodeError:
             print("Error")
-            
-        ausgeben(line)
 except KeyboardInterrupt:
     s.close()
+
+
 
 
 
